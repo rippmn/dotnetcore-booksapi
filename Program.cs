e82739735016cf95a26581005a964ae4691463ed
ripka <ripka@google.com>
2020-09-25 01:52:36 +0000

    set port

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace BooksApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+		    webBuilder.UseUrls("http://*:8080/");
                 });
     }
 }
